08b9338991cec5cf546d15f391256092e702ded7
EsiMohammadzadeh <muhmmadzade1995@outlook.com>
2023-10-17 21:27:00 +0330

    added the styles and scripts of services component

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
   navbar.classList.toggle("nav__expanded");
 });
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    // reomve previous active classes
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContents) =>
+      tabContents.classList.remove("active")
+    );
+
+    // add new active classes
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
